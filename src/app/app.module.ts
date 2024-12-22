7e2d9ed1d352367205050e97c4e4fe67bd1ecd69
Mario Garcia <msgimanila@yahoo.com>
2024-12-22 22:37:51 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {Component, inject} from '@angular/core';
+import {
+  MatBottomSheet,
+  MatBottomSheetModule,
+  MatBottomSheetRef,
+} from '@angular/material/bottom-sheet';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+
+/**
+ * @title Bottom Sheet Overview
+ */
+@Component({
+  selector: 'bottom-sheet-overview-example',
+  templateUrl: 'bottom-sheet-overview-example.html',
+  imports: [MatButtonModule, MatBottomSheetModule],
+})
+export class BottomSheetOverviewExample {
+  private _bottomSheet = inject(MatBottomSheet);
+
+  openBottomSheet(): void {
+    this._bottomSheet.open(BottomSheetOverviewExampleSheet);
+  }
+}
+
+@Component({
+  selector: 'bottom-sheet-overview-example-sheet',
+  templateUrl: 'bottom-sheet-overview-example-sheet.html',
+  imports: [MatListModule],
+})
+export class BottomSheetOverviewExampleSheet {
+  private _bottomSheetRef =
+    inject<MatBottomSheetRef<BottomSheetOverviewExampleSheet>>(MatBottomSheetRef);
+
+  openLink(event: MouseEvent): void {
+    this._bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
+}
 
 @NgModule({
   declarations: [
